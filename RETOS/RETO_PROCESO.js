9d39e425b785a37e623c63cc4eab2d0edfacffb6
Kenneth32Mena <kenneth2024@hotmail.com>
2026-09-14 14:36:03 -0600

    cambio

--- RETOS/RETO_PROCESO.js
+++ RETOS/RETO_PROCESO.js
@@ -1,7 +1,8 @@
 process.stdout.write(`Hola ${process.argv[2]},¿Cuantos años tienes? `)
 process.stdin.on('data', data => {
-    process.stdout.write(`${process.argv[2]} , tienes ${data} años!`)
+    process.stdout.write(`Hola ${process.argv[2]}, tienes ${String(data).trim()} años!\n`)
+    process.exit();
 });
 process.on('exit', () => {
-    process.stdout.write(`\nprograma .....\n`)
+    process.stdout.write(`\nFin del programa.....\n`)
 });
